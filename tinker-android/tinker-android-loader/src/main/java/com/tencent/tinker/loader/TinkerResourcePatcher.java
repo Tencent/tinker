98f3533c2cb61212f3936236c5394e9214e594ee
tomystang <tomystang@tencent.com>
2017-02-09 13:44:22 +0800

    [tinker] tidy up codes.

--- tinker-android/tinker-android-loader/src/main/java/com/tencent/tinker/loader/TinkerResourcePatcher.java
+++ tinker-android/tinker-android-loader/src/main/java/com/tencent/tinker/loader/TinkerResourcePatcher.java
@@ -43,13 +43,11 @@ import static android.os.Build.VERSION_CODES.KITKAT;
 class TinkerResourcePatcher {
     private static final String TAG                     = "Tinker.ResourcePatcher";
     private static final String TEST_ASSETS_VALUE       = "only_use_to_test_tinker_resource.txt";
-//    private static final String MIUI_RESOURCE_CLASSNAME = "android.content.res.MiuiResources";
 
     // original object
     private static Collection<WeakReference<Resources>>  references               = null;
     private static Object                                currentActivityThread    = null;
     private static AssetManager                          newAssetManager          = null;
-    //    private static ArrayMap<?, WeakReference<?>>         resourceImpls            = null;
 
     // method
     private static Method                                addAssetPathMethod       = null;
@@ -61,9 +59,6 @@ class TinkerResourcePatcher {
     private static Field resDir                = null;
     private static Field packagesFiled         = null;
     private static Field resourcePackagesFiled = null;
-//    private static Field        publicSourceDirField     = null;
-
-//    private static boolean isMiuiSystem = false;
 
     public static void isResourceCanPatch(Context context) throws Throwable {
         //   - Replace mResDir to point to the external resource file instead of the .apk. This is
@@ -127,8 +122,6 @@ class TinkerResourcePatcher {
                 // N moved the resources to mResourceReferences
                 Field mResourceReferences = resourcesManagerClass.getDeclaredField("mResourceReferences");
                 mResourceReferences.setAccessible(true);
-//                resourceImpls = (ArrayMap<?, WeakReference<?>>) mResourceReferences.get("mResourceImpls");
-
                 references = (Collection<WeakReference<Resources>>) mResourceReferences.get(resourcesManager);
             }
         } else {
@@ -150,15 +143,6 @@ class TinkerResourcePatcher {
             resourcesImplFiled = Resources.class.getDeclaredField("mResourcesImpl");
             resourcesImplFiled.setAccessible(true);
         }
-
-//        final Resources resources = context.getResources();
-//        isMiuiSystem = resources != null && MIUI_RESOURCE_CLASSNAME.equals(resources.getClass().getName());
-
-//        try {
-//            publicSourceDirField = ShareReflectUtil.findField(ApplicationInfo.class, "publicSourceDir");
-//        } catch (NoSuchFieldException e) {
-//            throw new IllegalStateException("cannot find 'mInstrumentation' field");
-//        }
     }
 
     /**
@@ -216,11 +200,6 @@ class TinkerResourcePatcher {
             }
         }
 
-        // Handle issues caused by WebView on Android N.
-        // Issue: On Android N, if an activity contains a webview, when screen rotates
-        // our resource patch may lost effects.
-//        publicSourceDirField.set(context.getApplicationInfo(), externalResourceFile);
-
         if (!checkResUpdate(context)) {
             throw new TinkerRuntimeException(ShareConstants.CHECK_RES_INSTALL_FAIL);
         }
@@ -232,12 +211,6 @@ class TinkerResourcePatcher {
      * MiuiResource change TypedArray to MiuiTypedArray, but it get string block from offset instead of assetManager
      */
     private static void clearPreloadTypedArrayIssue(Resources resources) {
-        // Perform this trick not only in Miui system since we can't predict if any other
-        // manufacturer would do the same modification to Android.
-//        if (!isMiuiSystem) {
-//            return;
-//        }
-
         Log.w(TAG, "try to clear typedArray cache!");
         // Clear typedArray cache.
         try {
