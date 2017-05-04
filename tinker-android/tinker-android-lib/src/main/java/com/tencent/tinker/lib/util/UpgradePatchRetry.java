f4a883926a6fd8295ca519474b98b5051a55fe7d
shwenzhang <shwenzhang@tencent.com>
2017-05-04 11:36:54 +0800

    edit tinker version to 1.7.10

--- tinker-android/tinker-android-lib/src/main/java/com/tencent/tinker/lib/util/UpgradePatchRetry.java
+++ tinker-android/tinker-android-lib/src/main/java/com/tencent/tinker/lib/util/UpgradePatchRetry.java
@@ -45,7 +45,7 @@ public class UpgradePatchRetry {
 
     private static final String RETRY_FILE_MD5_PROPERTY = "md5";
     private static final String RETRY_COUNT_PROPERTY    = "times";
-    private static final int    RETRY_MAX_COUNT         = 4;
+    private static final int    RETRY_MAX_COUNT         = 3;
     private static UpgradePatchRetry sInstance;
     private boolean isRetryEnable = true;
     private File    retryInfoFile = null;
