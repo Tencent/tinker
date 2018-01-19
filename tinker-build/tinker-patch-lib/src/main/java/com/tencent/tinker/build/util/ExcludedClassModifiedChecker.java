9f9ca820281aa2abcdae8d84f10efa7d60fee79f
tomystang <tomystang@tencent.com>
2018-01-19 11:23:44 +0800

    [tinker] allow remove loader classes.

--- tinker-build/tinker-patch-lib/src/main/java/com/tencent/tinker/build/util/ExcludedClassModifiedChecker.java
+++ tinker-build/tinker-patch-lib/src/main/java/com/tencent/tinker/build/util/ExcludedClassModifiedChecker.java
@@ -90,7 +90,7 @@ public final class ExcludedClassModifiedChecker {
                  * 2. Primary new dex is missing.
                  * 3. There are not any loader classes in primary old dex.
                  * 4. There are some new loader classes added in new primary dex.
-                 * 5. Loader classes in old primary dex are modified, deleted in new primary dex.
+                 * 5. Loader classes in old primary dex are modified in new primary dex.
                  * 6. Loader classes are found in secondary old dexes.
                  * 7. Loader classes are found in secondary new dexes.
                  */
@@ -112,7 +112,7 @@ public final class ExcludedClassModifiedChecker {
                             if (deletedClassInfos.isEmpty() && changedClassInfosMap.isEmpty() && !addedClassInfos.isEmpty()) {
                                 stmCode = STMCODE_ERROR_LOADER_CLASS_NOT_IN_PRIMARY_OLD_DEX;
                             } else {
-                                if (deletedClassInfos.isEmpty() && addedClassInfos.isEmpty()) {
+                                if (addedClassInfos.isEmpty()) {
                                     // class descriptor is completely matches, see if any contents changes.
                                     ArrayList<String> removeClasses = new ArrayList<>();
                                     for (String classname : changedClassInfosMap.keySet()) {
@@ -185,10 +185,8 @@ public final class ExcludedClassModifiedChecker {
                 }
                 case STMCODE_ERROR_LOADER_CLASS_IN_PRIMARY_DEX_MISMATCH: {
                     throw new TinkerPatchException(
-                        "loader classes in old primary dex are mismatched to those in new primary dex, \n"
-                            + "if deleted classes is not empty, check if your dex division strategy is fine. \n"
-                            + "added classes: " + Utils.collectionToString(addedClassInfos) + "\n"
-                            + "deleted classes: " + Utils.collectionToString(deletedClassInfos)
+                        "there's loader classes added in new primary dex, such these changes will not take effect.\n"
+                            + "added classes: " + Utils.collectionToString(addedClassInfos)
                     );
                 }
                 case STMCODE_ERROR_LOADER_CLASS_FOUND_IN_SECONDARY_OLD_DEX: {
@@ -199,7 +197,7 @@ public final class ExcludedClassModifiedChecker {
                 }
                 case STMCODE_ERROR_LOADER_CLASS_CHANGED: {
                     String msg =
-                        "some loader class has been changed in new dex."
+                        "some loader class has been changed in new primary dex."
                             + " Such these changes will not take effect!!"
                             + " related classes: "
                             + Utils.collectionToString(changedClassInfosMap.keySet());
