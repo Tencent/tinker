e10c5591d60e0773e4f72366861e3cdbf5350e7c
李樟取 <lizhangqu@weidian.com>
2018-08-04 21:40:21 +0800

    * 添加判断aapt2是否启用的兼容方法

--- tinker-build/tinker-patch-gradle-plugin/src/main/groovy/com/tencent/tinker/build/gradle/task/TinkerResourceIdTask.groovy
+++ tinker-build/tinker-patch-gradle-plugin/src/main/groovy/com/tencent/tinker/build/gradle/task/TinkerResourceIdTask.groovy
@@ -23,6 +23,7 @@ import com.tencent.tinker.build.aapt.RDotTxtEntry
 import com.tencent.tinker.build.gradle.TinkerPatchPlugin
 import com.tencent.tinker.build.util.FileOperation
 import org.gradle.api.DefaultTask
+import org.gradle.api.Project
 import org.gradle.api.tasks.TaskAction
 
 /**
@@ -45,6 +46,74 @@ public class TinkerResourceIdTask extends DefaultTask {
         group = 'tinker'
     }
 
+    /**
+     * 获取Android Gradle Plugin版本号
+     */
+    static String getAndroidGradlePluginVersionCompat() {
+        String version = null
+        try {
+            Class versionModel = Class.forName("com.android.builder.model.Version")
+            def versionFiled = versionModel.getDeclaredField("ANDROID_GRADLE_PLUGIN_VERSION")
+            versionFiled.setAccessible(true)
+            version = versionFiled.get(null)
+        } catch (Exception e) {
+
+        }
+        return version
+    }
+
+    /**
+     * 反射获取枚举类，静态方法
+     */
+    static <T> T resolveEnumValue(String value, Class<T> type) {
+        for (T constant : type.getEnumConstants()) {
+            if (constant.toString().equalsIgnoreCase(value)) {
+                return constant
+            }
+        }
+        return null
+    }
+
+    /**
+     * 获取ProjectOptions
+     */
+    static def getProjectOptions(Project project) {
+        try {
+            def basePlugin = project.getPlugins().hasPlugin('com.android.application') ? project.getPlugins().findPlugin('com.android.application') : project.getPlugins().findPlugin('com.android.library')
+            return Class.forName("com.android.build.gradle.BasePlugin").getMetaClass().getProperty(basePlugin, 'projectOptions')
+        } catch (Exception e) {
+        }
+        return null
+    }
+
+    /**
+     * 判断是否启用了aapt2
+     */
+    static boolean isAapt2Enabled(Project project) {
+        if (getAndroidGradlePluginVersionCompat() >= '3.3.0') {
+            //3.3.0以上默认开启，不再有开关
+            return true
+        }
+        boolean aapt2Enabled = false
+        try {
+            def projectOptions = getProjectOptions(project)
+            Object enumValue = resolveEnumValue("ENABLE_AAPT2", Class.forName("com.android.build.gradle.options.BooleanOption"))
+            aapt2Enabled = projectOptions.get(enumValue)
+        } catch (Exception e) {
+            try {
+                //gradle 2.3.3的方法
+                Class classAndroidGradleOptions = Class.forName("com.android.build.gradle.AndroidGradleOptions")
+                def isAapt2Enabled = classAndroidGradleOptions.getDeclaredMethod("isAapt2Enabled", Project.class)
+                isAapt2Enabled.setAccessible(true)
+                aapt2Enabled = isAapt2Enabled.invoke(null, project)
+            } catch (Exception e1) {
+                //都取不到表示还不支持
+                aapt2Enabled = false
+            }
+        }
+        return aapt2Enabled
+    }
+    
     @TaskAction
     def applyResourceId() {
         String resourceMappingFile = project.extensions.tinkerPatch.buildConfig.applyResourceMapping
