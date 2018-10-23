f88d3b409b55c81e2f03375591df6364ae113ee3
tomystang <yinshengtang92@gmail.com>
2018-10-23 20:07:32 +0800

    [tinker] re-implement AbstractResultService using JobSchedulerService to make tinker compatible with Android O and newer system.

--- tinker-android/tinker-android-lib/src/main/java/com/tencent/tinker/lib/service/AbstractResultService.java
+++ tinker-android/tinker-android-lib/src/main/java/com/tencent/tinker/lib/service/AbstractResultService.java
@@ -16,46 +16,52 @@
 
 package com.tencent.tinker.lib.service;
 
-import android.app.IntentService;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import com.tencent.tinker.lib.util.TinkerJobIntentService;
 
 import com.tencent.tinker.lib.util.TinkerLog;
+import com.tencent.tinker.loader.BuildConfig;
 import com.tencent.tinker.loader.TinkerRuntimeException;
 import com.tencent.tinker.loader.shareutil.ShareIntentUtil;
 
 /**
  * Created by zhangshaowen on 16/3/14.
  */
-public abstract class AbstractResultService extends IntentService {
+public abstract class AbstractResultService extends TinkerJobIntentService {
     private static final String TAG = "Tinker.AbstractResultService";
+    private static final int JOB_ID = 0xf3f4f5f6;
 
     private static final String RESULT_EXTRA = "result_extra";
-
-
-    /**
-     * Creates an IntentService.  Invoked by your subclass's constructor.
-     */
-    public AbstractResultService() {
-        super(AbstractResultService.class.getSimpleName());
-    }
 
     public static void runResultService(Context context, PatchResult result, String resultServiceClass) {
         if (resultServiceClass == null) {
             throw new TinkerRuntimeException("resultServiceClass is null.");
         }
         try {
+            if (!TinkerJobIntentService.class.isAssignableFrom(Class.forName(resultServiceClass))) {
+                throw new TinkerRuntimeException("on tinker version " + BuildConfig.TINKER_VERSION + " result service class must inherit from TinkerJobIntentService.");
+            }
+        } catch (ClassNotFoundException e) {
+            throw new TinkerRuntimeException("cannot find result service class: " + resultServiceClass, e);
+        }
+        try {
             Intent intent = new Intent();
             intent.setClassName(context, resultServiceClass);
             intent.putExtra(RESULT_EXTRA, result);
-
-            context.startService(intent);
+            enqueueWork(context, new ComponentName(context, resultServiceClass), JOB_ID, intent);
         } catch (Throwable throwable) {
             TinkerLog.e(TAG, "run result service fail, exception:" + throwable);
         }
     }
 
     @Override
+    protected void onHandleWork(@NonNull Intent intent) {
+        onHandleIntent(intent);
+    }
+
     protected void onHandleIntent(Intent intent) {
         if (intent == null) {
             TinkerLog.e(TAG, "AbstractResultService received a null intent, ignoring.");
