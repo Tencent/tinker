Tencent/tinker

commit 8be71815d8bfacae6ba0ccba43244b52c18d1c86

fixes #1084 add option to enable trimming dex to remove loader classes for every dex; add option to suppress the loader classes existence in any dex.
Signed-off-by: noproxy <toxzcp@gmail.com>

tinker-build/tinker-patch-lib/src/main/java/com/tencent/tinker/build/util/ExcludedClassModifiedChecker.java
@@ -190,10 +190,20 @@ public final class ExcludedClassModifiedChecker {
                     );
                 }
                 case STMCODE_ERROR_LOADER_CLASS_FOUND_IN_SECONDARY_OLD_DEX: {
-                    throw new TinkerPatchException("loader classes are found in old secondary dex. Found classes: " + Utils.collectionToString(oldClassesDescToCheck));
+                    final String msg = "loader classes are found in old secondary dex. Found classes: " + Utils.collectionToString(oldClassesDescToCheck);
+                    if (config.mAllowLoaderInAnyDex) {
+                        Logger.d(msg);
+                    } else {
+                        throw new TinkerPatchException(msg);
+                    }
                 }
                 case STMCODE_ERROR_LOADER_CLASS_FOUND_IN_SECONDARY_NEW_DEX: {
-                    throw new TinkerPatchException("loader classes are found in new secondary dex. Found classes: " + Utils.collectionToString(newClassesDescToCheck));
+                    final String msg = "loader classes are found in new secondary dex. Found classes: " + Utils.collectionToString(newClassesDescToCheck);
+                    if (config.mAllowLoaderInAnyDex) {
+                        Logger.d(msg);
+                    } else {
+                        throw new TinkerPatchException(msg);
+                    }
                 }
                 case STMCODE_ERROR_LOADER_CLASS_CHANGED: {
                     String msg =

tinker-build/tinker-patch-gradle-plugin/src/main/groovy/com/tencent/tinker/build/gradle/extension/TinkerPatchExtension.groovy
@@ -49,6 +49,42 @@ public class TinkerPatchExtension {
     boolean ignoreWarning
 
     /**
+     *
+     * Allow loader class existence in any class loader.
+     *
+     * This will suppress the exception like:
+     * <pre>
+     * loader classes are found in old secondary dex. Found classes: ...
+     * loader classes are found in new secondary dex. Found classes: ...
+     * </pre>
+     *
+     * <p>Since Android Gradle Plugin 3.3.0, there is no simply way to keep all loader classes in
+     * the primary dex file if your application's min sdk version is 21 or above. In this situation, you
+     * can turn the {@link #removeLoaderForAllDex} and {@link #allowLoaderInAnyDex} to true and
+     * tolerate the loader classes to exists in any dex file.
+     *
+     * <p>default: false
+     */
+    boolean allowLoaderInAnyDex
+
+    /**
+     * Whether to remove loader class for every dex file. When false, we will assume the loader
+     * class only exists in the main dex(classes.dex).
+     *
+     * <p>If the loader class may exists in any dex, you must set this to true. Otherwise, you patch will
+     * cause tinker runtime load failed. But this will cause a little increment on the size
+     * of the patch file.
+     *
+     * <p>Since Android Gradle Plugin 3.3.0, there is no simply way to keep all loader classes in
+     * the primary dex file if your application's min sdk version is 21 or above. In this situation, you
+     * can turn the {@link #removeLoaderForAllDex} and {@link #allowLoaderInAnyDex} to true and
+     * tolerate the loader classes to exists in any dex file.
+     *
+     * <p>default: false
+     */
+    boolean removeLoaderForAllDex
+
+    /**
      * If sign the patch file with the android signConfig
      * default: true
      */
@@ -65,6 +101,8 @@ public class TinkerPatchExtension {
         outputFolder = ""
         newApk = ""
         ignoreWarning = false
+        allowLoaderInAnyDex = false
+        removeLoaderForAllDex = false
         useSign = true
         tinkerEnable = true
     }
@@ -88,6 +126,7 @@ public class TinkerPatchExtension {
            | outputFolder = ${outputFolder}
            | newApk = ${newApk}
            | ignoreWarning = ${ignoreWarning}
+           | removeLoaderForAllDex = ${removeLoaderForAllDex}
            | tinkerEnable = ${tinkerEnable}
            | useSign = ${useSign}
         """.stripMargin()

tinker-build/tinker-patch-gradle-plugin/src/main/groovy/com/tencent/tinker/build/gradle/task/TinkerPatchSchemaTask.groovy
@@ -15,6 +15,8 @@
  */
 
 package com.tencent.tinker.build.gradle.task
+
+import com.tencent.tinker.build.gradle.extension.TinkerPatchExtension
 import com.tencent.tinker.build.patch.InputParam
 import com.tencent.tinker.build.patch.Runner
 import org.gradle.api.DefaultTask
@@ -26,7 +28,7 @@ import org.gradle.api.tasks.TaskAction
  * @author zhangshaowen
  */
 public class TinkerPatchSchemaTask extends DefaultTask {
-    def configuration
+    TinkerPatchExtension configuration
     def android
     String buildApkPath
     String outputFolder
@@ -66,6 +68,8 @@ public class TinkerPatchSchemaTask extends DefaultTask {
                .setNewApk(buildApkPath)
                .setOutBuilder(outputFolder)
                .setIgnoreWarning(configuration.ignoreWarning)
+               .setAllowLoaderInAnyDex(configuration.allowLoaderInAnyDex)
+               .setRemoveLoaderForAllDex(configuration.removeLoaderForAllDex)
                .setDexFilePattern(new ArrayList<String>(configuration.dex.pattern))
                .setIsProtectedApp(configuration.buildConfig.isProtectedApp)
                .setIsComponentHotplugSupported(configuration.buildConfig.supportHotplugComponent)

tinker-build/tinker-patch-lib/src/main/java/com/tencent/tinker/build/decoder/DexDiffDecoder.java
@@ -399,8 +399,12 @@ public class DexDiffDecoder extends BaseDecoder {
                 if (realClassNDexFiles.contains(oldDexFile)) {
                     // Bugfix: However, if what we would copy directly is main dex, we should do an additional diff operation
                     // so that patch applier would help us remove all loader classes of it in runtime.
-                    if (dexName.equals(DexFormat.DEX_IN_JAR_NAME)) {
-                        Logger.d("\nDo additional diff on main dex to remove loader classes in it.");
+                    if (config.mRemoveLoaderForAllDex || dexName.equals(DexFormat.DEX_IN_JAR_NAME)) {
+                        if (config.mRemoveLoaderForAllDex) {
+                            Logger.d("\nDo additional diff on every dex to remove loader classes in it, because removeLoaderForAllDex = true");
+                        } else {
+                            Logger.d("\nDo additional diff on main dex to remove loader classes in it.");
+                        }
                         diffDexPairAndFillRelatedInfo(oldDexFile, newDexFile, relatedInfo);
                         logToDexMeta(newDexFile, oldDexFile, relatedInfo.dexDiffFile, relatedInfo.newOrFullPatchedMd5, relatedInfo.newOrFullPatchedMd5, relatedInfo.dexDiffMd5, relatedInfo.newOrFullPatchedCRC);
                     } else {

tinker-build/tinker-patch-lib/src/main/java/com/tencent/tinker/build/patch/Configuration.java
@@ -62,6 +62,8 @@ public class Configuration {
     protected static final String ATTR_NAME  = "name";
 
     protected static final String ATTR_IGNORE_WARNING            = "ignoreWarning";
+    protected static final String ATTR_ALLOW_LOADER_IN_ANY_DEX   = "allowLoaderInAnyDex";
+    protected static final String ATTR_REMOVE_LOADER_FOR_ALL_DEX = "removeLoaderForAllDex";
     protected static final String ATTR_IS_PROTECTED_APP          = "isProtectedApp";
     protected static final String ATTR_SUPPORT_HOTPLUG_COMPONENT = "supportHotplugComponent";
     protected static final String ATTR_USE_SIGN                  = "useSign";
@@ -88,7 +90,9 @@ public class Configuration {
     public File    mOldApkFile;
     public File    mNewApkFile;
     public boolean mIgnoreWarning;
+    public boolean mAllowLoaderInAnyDex;
     public boolean mIsProtectedApp;
+    public boolean mRemoveLoaderForAllDex;
     public boolean mSupportHotplugComponent;
     /**
      * lib config
@@ -232,6 +236,10 @@ public class Configuration {
 
         mIgnoreWarning = param.ignoreWarning;
 
+        mAllowLoaderInAnyDex = param.allowLoaderInAnyDex;
+
+        mRemoveLoaderForAllDex= param.removeLoaderForAllDex;
+
         mIsProtectedApp = param.isProtectedApp;
 
         mSupportHotplugComponent = param.supportHotplugComponent;
@@ -257,6 +265,8 @@ public class Configuration {
         sb.append("newApk:" + mNewApkPath + "\n");
         sb.append("outputFolder:" + mOutFolder + "\n");
         sb.append("isIgnoreWarning:" + mIgnoreWarning + "\n");
+        sb.append("isAllowLoaderClassInAnyDex:" + mAllowLoaderInAnyDex + "\n");
+        sb.append("isRemoveLoaderForAllDex:" + mRemoveLoaderForAllDex + "\n");
         sb.append("isProtectedApp:" + mIsProtectedApp + "\n");
         sb.append("7-ZipPath:" + mSevenZipPath + "\n");
         sb.append("useSignAPk:" + mUseSignAPk + "\n");
@@ -439,6 +449,10 @@ public class Configuration {
                     }
                     if (tagName.equals(ATTR_IGNORE_WARNING)) {
                         mIgnoreWarning = value.equals("true");
+                    } else if (tagName.equals(ATTR_ALLOW_LOADER_IN_ANY_DEX)) {
+                        mAllowLoaderInAnyDex = value.equals("true");
+                    } else if (tagName.equals(ATTR_REMOVE_LOADER_FOR_ALL_DEX)) {
+                        mRemoveLoaderForAllDex = value.equals("true");
                     } else if (tagName.equals(ATTR_IS_PROTECTED_APP)) {
                         mIsProtectedApp = value.equals("true");
                     } else if (tagName.equals(ATTR_SUPPORT_HOTPLUG_COMPONENT)) {

tinker-build/tinker-patch-lib/src/main/java/com/tencent/tinker/build/patch/InputParam.java
@@ -35,6 +35,8 @@ public class InputParam {
     public final String  storealias;
     public final String  storepass;
     public final boolean ignoreWarning;
+    public final boolean allowLoaderInAnyDex;
+    public final boolean removeLoaderForAllDex;
     public final boolean isProtectedApp;
     public final boolean supportHotplugComponent;
     public final boolean useSign;
@@ -81,32 +83,34 @@ public class InputParam {
     public final String                  sevenZipPath;
 
     private InputParam(
-        String oldApk,
-        String newApk,
-        String outFolder,
-        File signFile,
-        String keypass,
-        String storealias,
-        String storepass,
-        boolean ignoreWarning,
-        boolean isProtectedApp,
-        boolean supportHotplugComponent,
-        boolean useSign,
+            String oldApk,
+            String newApk,
+            String outFolder,
+            File signFile,
+            String keypass,
+            String storealias,
+            String storepass,
+            boolean ignoreWarning,
+            boolean allowLoaderInAnyDex,
+            boolean removeLoaderForAllDex,
+            boolean isProtectedApp,
+            boolean supportHotplugComponent,
+            boolean useSign,
 
-        ArrayList<String> dexFilePattern,
-        ArrayList<String> dexLoaderPattern,
-        ArrayList<String> dexIgnoreChangeLoaderPattern,
+            ArrayList<String> dexFilePattern,
+            ArrayList<String> dexLoaderPattern,
+            ArrayList<String> dexIgnoreChangeLoaderPattern,
 
-        String dexMode,
-        ArrayList<String> soFilePattern,
-        ArrayList<String> resourceFilePattern,
-        ArrayList<String> resourceIgnoreChangePattern,
-        ArrayList<String> resourceIgnoreChangeWarningPattern,
-        int largeModSize,
-        boolean useApplyResource,
-        HashMap<String, String> configFields,
+            String dexMode,
+            ArrayList<String> soFilePattern,
+            ArrayList<String> resourceFilePattern,
+            ArrayList<String> resourceIgnoreChangePattern,
+            ArrayList<String> resourceIgnoreChangeWarningPattern,
+            int largeModSize,
+            boolean useApplyResource,
+            HashMap<String, String> configFields,
 
-        String sevenZipPath
+            String sevenZipPath
     ) {
         this.oldApk = oldApk;
         this.newApk = newApk;
@@ -116,6 +120,8 @@ public class InputParam {
         this.storealias = storealias;
         this.storepass = storepass;
         this.ignoreWarning = ignoreWarning;
+        this.allowLoaderInAnyDex = allowLoaderInAnyDex;
+        this.removeLoaderForAllDex = removeLoaderForAllDex;
         this.isProtectedApp = isProtectedApp;
         this.supportHotplugComponent = supportHotplugComponent;
         this.useSign = useSign;
@@ -149,6 +155,8 @@ public class InputParam {
         private String  storealias;
         private String  storepass;
         private boolean ignoreWarning;
+        private boolean allowLoaderInAnyDex;
+        private boolean removeLoaderForAllDex;
         private boolean isProtectedApp;
         private boolean isComponentHotplugSupported;
         private boolean useSign;
@@ -273,6 +281,16 @@ public class InputParam {
             return this;
         }
 
+        public Builder setAllowLoaderInAnyDex(boolean allowLoaderInAnyDex) {
+            this.allowLoaderInAnyDex = allowLoaderInAnyDex;
+            return this;
+        }
+
+        public Builder setRemoveLoaderForAllDex(boolean removeLoaderForAllDex){
+            this.removeLoaderForAllDex = removeLoaderForAllDex;
+            return this;
+        }
+
         public Builder setIsProtectedApp(boolean isProtectedApp) {
             this.isProtectedApp = isProtectedApp;
             return this;
@@ -323,6 +341,8 @@ public class InputParam {
                     storealias,
                     storepass,
                     ignoreWarning,
+                    allowLoaderInAnyDex,
+                    removeLoaderForAllDex,
                     isProtectedApp,
                     isComponentHotplugSupported,
                     useSign,

commit 3bca569427b55d4adec07101f2321c81b02a1d5c

fix the missing `break` introduced by 8be71815d8bfacae6ba0ccba43244b52c18d1c86
Signed-off-by: noproxy <toxzcp@gmail.com>

tinker-build/tinker-patch-lib/src/main/java/com/tencent/tinker/build/util/ExcludedClassModifiedChecker.java
@@ -193,6 +193,7 @@ public final class ExcludedClassModifiedChecker {
                     final String msg = "loader classes are found in old secondary dex. Found classes: " + Utils.collectionToString(oldClassesDescToCheck);
                     if (config.mAllowLoaderInAnyDex) {
                         Logger.d(msg);
+                        break;
                     } else {
                         throw new TinkerPatchException(msg);
                     }
@@ -201,6 +202,7 @@ public final class ExcludedClassModifiedChecker {
                     final String msg = "loader classes are found in new secondary dex. Found classes: " + Utils.collectionToString(newClassesDescToCheck);
                     if (config.mAllowLoaderInAnyDex) {
                         Logger.d(msg);
+                        break;
                     } else {
                         throw new TinkerPatchException(msg);
                     }