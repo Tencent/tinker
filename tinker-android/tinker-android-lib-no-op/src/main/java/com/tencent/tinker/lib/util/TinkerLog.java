c4bb8ca6a0975dd0b1cb9b78f0483b326b64f420
tomystang <tomystang@tencent.com>
2020-04-22 15:26:29 +0800

    [tinker] Record pending logs before TinkerLog impl was set.

--- tinker-android/tinker-android-lib-no-op/src/main/java/com/tencent/tinker/lib/util/TinkerLog.java
+++ tinker-android/tinker-android-lib-no-op/src/main/java/com/tencent/tinker/lib/util/TinkerLog.java
@@ -16,6 +16,15 @@
 
 package com.tencent.tinker.lib.util;
 
+import android.util.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 /**
  * Created by zhangshaowen on 16/3/17.
  */
@@ -23,7 +32,11 @@ package com.tencent.tinker.lib.util;
 
 public class TinkerLog {
     private static final String TAG = "Tinker.TinkerLog";
-    private static TinkerLogImp debugLog = new TinkerLogImp() {
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.ENGLISH);
+
+    private static final List<Object[]> sPendingLogs = Collections.synchronizedList(new ArrayList<Object[]>());
+
+    private static final TinkerLogImp debugLog = new TinkerLogImp() {
 
         @Override
         public void v(final String tag, final String format, final Object... params) {
@@ -66,63 +79,144 @@ public class TinkerLog {
             android.util.Log.e(tag, log);
         }
     };
-    private static TinkerLogImp tinkerLogImp = debugLog;
+
+    // Guarded by itself.
+    private static final TinkerLogImp[] tinkerLogImpRef = {debugLog};
 
     public static void setTinkerLogImp(TinkerLogImp imp) {
-        tinkerLogImp = imp;
+        synchronized (tinkerLogImpRef) {
+            tinkerLogImpRef[0] = imp;
+        }
     }
 
     public static TinkerLogImp getImpl() {
-        return tinkerLogImp;
-    }
-
-    public static void v(final String tag, final String msg, final Object... obj) {
-        if (tinkerLogImp != null) {
-            tinkerLogImp.v(tag, msg, obj);
+        synchronized (tinkerLogImpRef) {
+            return tinkerLogImpRef[0];
         }
     }
 
-    public static void e(final String tag, final String msg, final Object... obj) {
-        if (tinkerLogImp != null) {
-            tinkerLogImp.e(tag, msg, obj);
+    public static void printPendingLogs() {
+        TinkerLogImp imp;
+        synchronized (tinkerLogImpRef) {
+            imp = tinkerLogImpRef[0];
+        }
+        if (sPendingLogs.isEmpty()) {
+            return;
+        }
+        if (imp == null) {
+            return;
+        }
+        for (Object[] log : sPendingLogs) {
+            final int level = (int) log[0];
+            final String tag = (String) log[1];
+            final String msg = (String) log[2];
+            switch (level) {
+                case Log.VERBOSE: {
+                    imp.v(tag, msg);
+                    break;
+                }
+                case Log.DEBUG: {
+                    imp.d(tag, msg);
+                    break;
+                }
+                case Log.INFO: {
+                    imp.i(tag, msg);
+                    break;
+                }
+                case Log.WARN: {
+                    imp.w(tag, msg);
+                    break;
+                }
+                case Log.ERROR: {
+                    imp.e(tag, msg);
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+        }
+        sPendingLogs.clear();
+    }
+
+    public static void v(final String tag, final String fmt, final Object... obj) {
+        final TinkerLogImp imp = getImpl();
+        if (imp != null) {
+            imp.v(tag, fmt, obj);
+        } else {
+            final String msg = (obj != null && obj.length > 0) ? String.format(fmt, obj) : fmt;
+            final String prefix = "[PendingLog@" + SIMPLE_DATE_FORMAT.format(new Date()) + "] ";
+            sPendingLogs.add(new Object[] {Log.VERBOSE, tag, prefix + msg});
         }
     }
 
-    public static void w(final String tag, final String msg, final Object... obj) {
-        if (tinkerLogImp != null) {
-            tinkerLogImp.w(tag, msg, obj);
+    public static void e(final String tag, final String fmt, final Object... obj) {
+        final TinkerLogImp imp = getImpl();
+        if (imp != null) {
+            imp.e(tag, fmt, obj);
+        } else {
+            final String msg = (obj != null && obj.length > 0) ? String.format(fmt, obj) : fmt;
+            final String prefix = "[PendingLog@" + SIMPLE_DATE_FORMAT.format(new Date()) + "] ";
+            sPendingLogs.add(new Object[] {Log.ERROR, tag, prefix + msg});
         }
     }
 
-    public static void i(final String tag, final String msg, final Object... obj) {
-        if (tinkerLogImp != null) {
-            tinkerLogImp.i(tag, msg, obj);
+    public static void w(final String tag, final String fmt, final Object... obj) {
+        final TinkerLogImp imp = getImpl();
+        if (imp != null) {
+            imp.w(tag, fmt, obj);
+        } else {
+            final String msg = (obj != null && obj.length > 0) ? String.format(fmt, obj) : fmt;
+            final String prefix = "[PendingLog@" + SIMPLE_DATE_FORMAT.format(new Date()) + "] ";
+            sPendingLogs.add(new Object[] {Log.WARN, tag, prefix + msg});
         }
     }
 
-    public static void d(final String tag, final String msg, final Object... obj) {
-        if (tinkerLogImp != null) {
-            tinkerLogImp.d(tag, msg, obj);
+    public static void i(final String tag, final String fmt, final Object... obj) {
+        final TinkerLogImp imp = getImpl();
+        if (imp != null) {
+            imp.i(tag, fmt, obj);
+        } else {
+            final String msg = (obj != null && obj.length > 0) ? String.format(fmt, obj) : fmt;
+            final String prefix = "[PendingLog@" + SIMPLE_DATE_FORMAT.format(new Date()) + "] ";
+            sPendingLogs.add(new Object[] {Log.INFO, tag, prefix + msg});
+        }
+    }
+
+    public static void d(final String tag, final String fmt, final Object... obj) {
+        final TinkerLogImp imp = getImpl();
+        if (imp != null) {
+            imp.d(tag, fmt, obj);
+        } else {
+            final String msg = (obj != null && obj.length > 0) ? String.format(fmt, obj) : fmt;
+            final String prefix = "[PendingLog@" + SIMPLE_DATE_FORMAT.format(new Date()) + "] ";
+            sPendingLogs.add(new Object[] {Log.DEBUG, tag, prefix + msg});
         }
     }
 
     public static void printErrStackTrace(String tag, Throwable tr, final String format, final Object... obj) {
-        if (tinkerLogImp != null) {
-            tinkerLogImp.printErrStackTrace(tag, tr, format, obj);
+        final TinkerLogImp imp = getImpl();
+        if (imp != null) {
+            imp.printErrStackTrace(tag, tr, format, obj);
+        } else {
+            String msg = (obj == null || obj.length == 0) ? format : String.format(format, obj);
+            msg += "  " + android.util.Log.getStackTraceString(tr);
+            final String prefix = "[PendingLog@" + SIMPLE_DATE_FORMAT.format(new Date()) + "] ";
+            sPendingLogs.add(new Object[] {Log.ERROR, tag, prefix + msg});
         }
     }
 
     public interface TinkerLogImp {
 
-        void v(final String tag, final String msg, final Object... obj);
+        void v(final String tag, final String fmt, final Object... obj);
 
-        void i(final String tag, final String msg, final Object... obj);
+        void i(final String tag, final String fmt, final Object... obj);
 
-        void w(final String tag, final String msg, final Object... obj);
+        void w(final String tag, final String fmt, final Object... obj);
 
-        void d(final String tag, final String msg, final Object... obj);
+        void d(final String tag, final String fmt, final Object... obj);
 
-        void e(final String tag, final String msg, final Object... obj);
+        void e(final String tag, final String fmt, final Object... obj);
 
         void printErrStackTrace(String tag, Throwable tr, final String format, final Object... obj);
 
