636dc40b7c1f6519f48c8e11920c8f2d3976e3dd
tangyinsheng <yinshengtang@foxmail.com>
2022-06-10 21:12:42 +0800

    [tinker] Add insurance hack logic for unexpected changes in resources.apk.

--- tinker-android/tinker-android-loader/src/main/java/com/tencent/tinker/loader/TinkerResourcePatcher.java
+++ tinker-android/tinker-android-loader/src/main/java/com/tencent/tinker/loader/TinkerResourcePatcher.java
@@ -16,32 +16,38 @@
 
 package com.tencent.tinker.loader;
 
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.KITKAT;
+import static com.tencent.tinker.loader.shareutil.ShareReflectUtil.findConstructor;
+import static com.tencent.tinker.loader.shareutil.ShareReflectUtil.findField;
+import static com.tencent.tinker.loader.shareutil.ShareReflectUtil.findMethod;
+
+import android.app.Application;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.res.AssetManager;
 import android.content.res.Resources;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
 import android.util.ArrayMap;
 import android.util.Log;
 
 import com.tencent.tinker.loader.shareutil.ShareConstants;
 import com.tencent.tinker.loader.shareutil.SharePatchFileUtil;
 import com.tencent.tinker.loader.shareutil.ShareReflectUtil;
+import com.tencent.tinker.loader.shareutil.ShareTinkerInternals;
 import com.tencent.tinker.loader.shareutil.ShareTinkerLog;
 
 import java.io.InputStream;
 import java.lang.ref.WeakReference;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
-import static android.os.Build.VERSION.SDK_INT;
-import static android.os.Build.VERSION_CODES.KITKAT;
-import static com.tencent.tinker.loader.shareutil.ShareReflectUtil.findConstructor;
-import static com.tencent.tinker.loader.shareutil.ShareReflectUtil.findField;
-import static com.tencent.tinker.loader.shareutil.ShareReflectUtil.findMethod;
 
 /**
  * Created by zhangshaowen on 16/9/21.
@@ -57,6 +63,8 @@ class TinkerResourcePatcher {
     private static AssetManager newAssetManager = null;
 
     // method
+    private static Constructor<?> newAssetManagerCtor = null;
+    private static Method isUpToDateMethod = null;
     private static Method addAssetPathMethod = null;
     private static Method addAssetPathAsSharedLibraryMethod = null;
     private static Method ensureStringBlocksMethod = null;
@@ -113,7 +121,13 @@ class TinkerResourcePatcher {
 
         // Use class fetched from instance to avoid some ROMs that use customized AssetManager
         // class. (e.g. Baidu OS)
-        newAssetManager = (AssetManager) findConstructor(assets).newInstance();
+        newAssetManagerCtor = findConstructor(assets);
+
+        try {
+            isUpToDateMethod = findMethod(assets, "isUpToDate");
+        } catch (Throwable ignored) {
+            ShareTinkerLog.w(TAG, "Fail to find isUpToDate method, insurance logic will be disabled.");
+        }
 
         // Iterate over all known Resources objects
         if (SDK_INT >= KITKAT) {
@@ -176,6 +190,17 @@ class TinkerResourcePatcher {
             return;
         }
 
+        if (newAssetManager != null) {
+            try {
+                if ((boolean) isUpToDateMethod.invoke(newAssetManager)) {
+                    return;
+                }
+                ShareTinkerLog.w(TAG, "Patched resources was changed unexpectly, re-inject now.");
+            } catch (Throwable thr) {
+                ShareTinkerLog.w(TAG, "Fail to judge if patched resources is up-to-date, app may crash soon.");
+            }
+        }
+
         final ApplicationInfo appInfo = context.getApplicationInfo();
 
         final Field[] packagesFields;
@@ -199,6 +224,8 @@ class TinkerResourcePatcher {
                 }
             }
         }
+
+        newAssetManager = (AssetManager) newAssetManagerCtor.newInstance();
 
         // Create a new AssetManager instance and point it to the resources installed under
         if (((Integer) addAssetPathMethod.invoke(newAssetManager, externalResourceFile)) == 0) {
@@ -264,6 +291,129 @@ class TinkerResourcePatcher {
 
         if (!checkResUpdate(context)) {
             throw new TinkerRuntimeException(ShareConstants.CHECK_RES_INSTALL_FAIL);
+        }
+
+        installResourceInsuranceHacks(context, externalResourceFile);
+    }
+
+    private static void installResourceInsuranceHacks(Context context, String patchedResApkPath) {
+        if (isUpToDateMethod == null) {
+            return;
+        }
+        try {
+            final Object activityThread = ShareReflectUtil.getActivityThread(context, null);
+            final Field mHField = ShareReflectUtil.findField(activityThread, "mH");
+            final Handler mH = (Handler) mHField.get(activityThread);
+            final Field mCallbackField = ShareReflectUtil.findField(Handler.class, "mCallback");
+            final Handler.Callback originCallback = (Handler.Callback) mCallbackField.get(mH);
+            if (!(originCallback instanceof ResourceInsuranceHandlerCallback)) {
+                final ResourceInsuranceHandlerCallback hackCallback = new ResourceInsuranceHandlerCallback(
+                        context, patchedResApkPath, originCallback, mH.getClass());
+                mCallbackField.set(mH, hackCallback);
+            } else {
+                ShareTinkerLog.w(TAG, "installResourceInsuranceHacks: already installed, skip rest logic.");
+            }
+        } catch (Throwable thr) {
+            ShareTinkerLog.printErrStackTrace(TAG, thr, "failed to install resource insurance hack.");
+        }
+    }
+
+    private static final class ResourceInsuranceHandlerCallback implements Handler.Callback {
+        private static final String LAUNCH_ACTIVITY_LIFECYCLE_ITEM_CLASSNAME = "android.app.servertransaction.LaunchActivityItem";
+
+        private final Context mContext;
+        private final String mPatchResApkPath;
+        private final Handler.Callback mOriginalCallback;
+
+        private final int LAUNCH_ACTIVITY;
+        private final int RELAUNCH_ACTIVITY;
+        private final int EXECUTE_TRANSACTION;
+
+        private Method mGetCallbacksMethod = null;
+        private boolean mSkipInterceptExecuteTransaction = false;
+
+        ResourceInsuranceHandlerCallback(Context context, String patchResApkPath, Handler.Callback original, Class<?> hClazz) {
+            Context appContext = context.getApplicationContext();
+            mContext = (appContext != null ? appContext : context);
+            mPatchResApkPath = patchResApkPath;
+            mOriginalCallback = original;
+            LAUNCH_ACTIVITY = fetchMessageId(hClazz, "LAUNCH_ACTIVITY", 100);
+            RELAUNCH_ACTIVITY = fetchMessageId(hClazz, "RELAUNCH_ACTIVITY", 126);
+
+            if (ShareTinkerInternals.isNewerOrEqualThanVersion(28, true)) {
+                EXECUTE_TRANSACTION  = fetchMessageId(hClazz, "EXECUTE_TRANSACTION ", 159);
+            } else {
+                EXECUTE_TRANSACTION = -1;
+            }
+        }
+
+        private int fetchMessageId(Class<?> hClazz, String name, int defVal) {
+            int value;
+            try {
+                value = ShareReflectUtil.findField(hClazz, name).getInt(null);
+            } catch (Throwable e) {
+                value = defVal;
+            }
+            return value;
+        }
+
+        @Override
+        public boolean handleMessage(Message msg) {
+            boolean consume = false;
+            if (hackMessage(msg)) {
+                consume = true;
+            } else if (mOriginalCallback != null) {
+                consume = mOriginalCallback.handleMessage(msg);
+            }
+            return consume;
+        }
+
+        @SuppressWarnings("unchecked")
+        private boolean hackMessage(Message msg) {
+            boolean shouldReInjectPatchedResources = false;
+            if (msg.what == LAUNCH_ACTIVITY || msg.what == RELAUNCH_ACTIVITY) {
+                shouldReInjectPatchedResources = true;
+            } else if (msg.what == EXECUTE_TRANSACTION) {
+                do {
+                    if (mSkipInterceptExecuteTransaction) {
+                        break;
+                    }
+                    final Object transaction = msg.obj;
+                    if (transaction == null) {
+                        ShareTinkerLog.w(TAG, "transaction is null, skip rest insurance logic.");
+                        break;
+                    }
+                    if (mGetCallbacksMethod == null) {
+                        try {
+                            mGetCallbacksMethod = ShareReflectUtil.findMethod(transaction, "getCallbacks");
+                        } catch (Throwable ignored) {
+                            // Ignored.
+                        }
+                    }
+                    if (mGetCallbacksMethod == null) {
+                        ShareTinkerLog.e(TAG, "fail to find getLifecycleStateRequest method, skip rest insurance logic.");
+                        mSkipInterceptExecuteTransaction = true;
+                        break;
+                    }
+                    try {
+                        final List<Object> req = (List<Object>) mGetCallbacksMethod.invoke(transaction);
+                        if (req != null && req.size() > 0) {
+                            final Object cb = req.get(0);
+                            shouldReInjectPatchedResources = cb != null && cb.getClass().getName().equals(LAUNCH_ACTIVITY_LIFECYCLE_ITEM_CLASSNAME);
+                        }
+                    } catch (Throwable ignored) {
+                        ShareTinkerLog.e(TAG, "fail to call getLifecycleStateRequest method, skip rest insurance logic.");
+                    }
+                } while (false);
+            }
+            if (shouldReInjectPatchedResources) {
+                try {
+                    monkeyPatchExistingResources(mContext, mPatchResApkPath);
+                } catch (Throwable thr) {
+                    ShareTinkerLog.printErrStackTrace(TAG, thr, "fail to ensure patched resources available after it's modified.");
+                }
+            }
+            return false;
         }
     }
 
