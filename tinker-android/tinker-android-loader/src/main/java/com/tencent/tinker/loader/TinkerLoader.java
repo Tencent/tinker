169434f2dca2f11b3a54371dc9b937958b0cf310
tomystang <tomystang@tencent.com>
2024-06-21 16:39:32 +0800

    [tinker] Fix secondary processes accidentally fallback to base version.

--- tinker-android/tinker-android-loader/src/main/java/com/tencent/tinker/loader/TinkerLoader.java
+++ tinker-android/tinker-android-loader/src/main/java/com/tencent/tinker/loader/TinkerLoader.java
@@ -335,11 +335,11 @@ public class TinkerLoader extends AbstractTinkerLoader {
                 resultIntent.putExtra(ShareIntentUtil.INTENT_PATCH_EXCEPTION, new TinkerRuntimeException("checkSafeModeCount fail"));
                 ShareIntentUtil.setIntentReturnCode(resultIntent, ShareConstants.ERROR_LOAD_PATCH_UNCAUGHT_EXCEPTION);
                 ShareTinkerLog.w(TAG, "tryLoadPatchFiles:checkSafeModeCount fail, patch was deleted.");
+                return;
             } else {
                 ShareTinkerLog.w(TAG, "tryLoadPatchFiles:checkSafeModeCount fail, but we are not in main process, mark the patch to be deleted and continue load patch.");
                 ShareTinkerInternals.cleanPatch(app);
             }
-            return;
         }
 
         //now we can load patch resource
